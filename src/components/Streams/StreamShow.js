321368030a9a40c5d9c09b4a7461709cdc220c27
Danilo <silva.danilo.nobre@gmail.com>
2019-05-12 07:10:18 -0300

    Add a FLV over WebSocket live stream playback in the ShowStream page

--- src/components/Streams/StreamShow.js
+++ src/components/Streams/StreamShow.js
@@ -1,10 +1,45 @@
 import React, { Component, Fragment } from "react";
 import { fetchStream } from "../../actions";
 import { connect } from "react-redux";
+import flv from "flv.js";
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.streamId);
+    const { streamId } = this.props.match.params;
+
+    this.props.fetchStream(streamId);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    const { streamId } = this.props.match.params;
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    if (flv.isSupported()) {
+      this.player = flv.createPlayer({
+        type: "flv",
+        url: `http://localhost:8000/live/${streamId}.flv`
+      });
+
+      this.player.attachMediaElement(this.videoRef.current);
+      this.player.load();
+    }
   }
 
   renderStream = () => {
@@ -15,6 +50,7 @@ class StreamShow extends Component {
     const { title, description } = this.props.stream;
     return (
       <Fragment>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </Fragment>
